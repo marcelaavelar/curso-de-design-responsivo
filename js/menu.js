14eeebc1065955fafb24b74c9d31b03c77355a3c
Marcela Avelar <marcela.lilian@gmail.com>
2018-08-13 22:41:38 -0300

    Correção de nivel de pastas

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+document.querySelector('.menu-abrir').onclick = function() {
+    document.documentElement.classList.add('menu-ativo');
+};
+
+document.querySelector('.menu-fechar').onclick = function() {
+    document.documentElement.classList.remove('.menu-ativo');
+};
+
+document.documentElement.onclick = function(even) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('.menu-ativo');
+    }
+};
+
+
